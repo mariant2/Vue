d48f946d3c2229ccb78fa3660ab4b815ded8d3c6
Mari Antoniol <104854007+mariant2@users.noreply.github.com>
2023-01-29 10:57:06 +0100

    Update routes.js

--- ecommerce/src/router/routes.js
+++ ecommerce/src/router/routes.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Vue from "vue"
 import Router from "vue-router"
-import Home from "./components.Home.vue"
+import Home from "./components/Home.vue"
 import Shop from './components/Shop.vue'
 import About from './components/About.vue'
 import Contact from './components/Contact.vue'
